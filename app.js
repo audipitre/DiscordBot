c5a114f5ed2d5dbe527611243108d0b4a10ccffe
Cal <callees18@hotmail.com>
2022-07-20 00:11:48 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const { token } = require('./config.json');
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(token);
