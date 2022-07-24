22479f287a9d88382440256b09b38d95ac42341f
Cal <callees18@hotmail.com>
2022-07-24 02:44:36 +0100

    Initial

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 var path = require('path');
 
-const { token } = require('./config.json');
+const { token, clientId } = require('./config.json');
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildPresences, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
@@ -9,6 +9,31 @@ let allTheMups = require(__dirname + '/MupsData/mup_counter.json');
 
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+const { REST } = require('@discordjs/rest'	);
+const { Routes } = require('discord.js');
+const rest = new REST({ version: '10'}).setToken(token);
+
+const commands = [];
+const slashCommands = require(__dirname +'\\Commands\\shaunCommands.js');
+
+commands.push(slashCommand.data.toJSON());
+client.commands.set(slashCommand.data.name, slashCommand);
+
+
+(async () => {
+	try {
+			await rest.put(
+				Routes.applicationCommands(clientId), {
+					body: commands
+				},
+			);
+			console.log('Successfully registered application commands globally');
+		 
+	} catch (error) {
+		if (error) console.error(error);
+	}
+})();
 
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
